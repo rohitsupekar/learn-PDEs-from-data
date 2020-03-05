39024302267e565a4a4421b4f6813a5f4ad76909
Rohit Supekar <rohit.supekar@gmail.com>
2020-03-05 15:12:01 -0500

    changed IHTd

--- solvers.py
+++ solvers.py
@@ -82,41 +82,44 @@ def STRidge(X, y, lam, tol, maxit=1000, W=None, standardize = False, print_flag 
 
     return w
 
+
 def IHTd(X, y, lasso_lam, max_iter, sub_iter, tol, htp_flag, print_flag=False):
     n,d = X.shape
-    coeff_old  = np.zeros((1,d))
-    coeff_new  = np.zeros((1,d))
+    coeff_old  = np.zeros((d,1))
+    coeff_new  = np.zeros((d,1))
     L = 1.0
     LL = np.linalg.norm(X.T.dot(X),2)
-    one  =  (1./L)*(X.T.dot(y)).reshape(d,)
+    one  =  (1./L)*(X.T.dot(y))
     XTX  = X.T.dot(X)
 
-    coeff_old[0,:] =  one #+ 0.01*np.random.normal(0, 1, d)
-    support =  np.arange(0,d).reshape(d,)
+    coeff_old =  one #+ 0.01*np.random.normal(0, 1, d)
+    support =  np.arange(0,d)
     frac_iter = 0
     time_step = (1.0/LL)
     for iteration in range(0, max_iter):
-        temp = np.zeros((1,d))
+        if print_flag:
+            print('iter = %i' %(iteration))
+        temp = np.zeros((d,1))
         if(htp_flag == 1):
-            temp[0,:] = coeff_old[0,:] + (2.0/LL) *(one  - XTX.dot(coeff_old[0,:])) #inclusion will make similar algorithm to HTP
+            temp = coeff_old + (2.0/LL) *(one  - XTX.dot(coeff_old)) #inclusion will make similar algorithm to HTP
         else:
-            temp[0,:] = coeff_old[0,:].copy()
+            temp = coeff_old.copy()
 
-        smallinds   = np.where( abs(temp[0,:]) <= lasso_lam/(L))[0]
+        smallinds   = np.where( abs(temp) <= lasso_lam/(L))[0]
         biginds     = [i for i in range(d) if i not in smallinds]
         if( len(biginds) == 0 ):  # if( len(biginds)==0 and iteration !=0 ):
-            coeff_new[0,:] = 0
-            return coeff_new.T
+            coeff_new[:] = 0
+            return coeff_new
         else:
             #temp    = np.zeros((1,d)) # added this
-            temper  = np.zeros((1,len(biginds)))
-            grad    = np.zeros((len(biginds),))
-            grad_A  = np.zeros((n,))
+            temper  = np.zeros((len(biginds), 1))
+            grad    = np.zeros((len(biginds),1))
+            grad_A  = np.zeros((n,1))
             LLL     = np.linalg.norm(X[:, biginds].T.dot(X[:, biginds]), 2)
-            XTy_s   = X[:, biginds].T.dot(y).reshape( len(biginds), )
+            XTy_s   = X[:, biginds].T.dot(y)
             XTX_s   = X[:, biginds].T.dot( X[:, biginds])
             for k in range(0, sub_iter):
-                temp_2      = XTX_s.dot( temp[0, biginds] ).reshape(len(biginds),)
+                temp_2      = XTX_s.dot( temp[biginds,:] )
                 grad        = (XTy_s - temp_2)
                 grad_A      = X[:, biginds].dot(grad)
                 if(grad.T.dot(grad) == 0):
@@ -124,27 +127,26 @@ def IHTd(X, y, lasso_lam, max_iter, sub_iter, tol, htp_flag, print_flag=False):
                 else:
                     time_step = (grad.T.dot(grad))/(grad_A.T.dot(grad_A))
 
-                temper[0,:] = temp[0,biginds] + (time_step)*grad
-                if( np.linalg.norm( y[:,0] - X[:, biginds].dot(temper[0,:])  , 2)**2 < (len(biginds)*lasso_lam) ):
-                    coeff_new[0, biginds]   = temper[0,:].copy()
-                    coeff_new[0, smallinds] = 0
+                temper = temp[biginds] + (time_step)*grad
+                if( np.linalg.norm( y - X[:, biginds].dot(temper), 2)**2 < (len(biginds)*lasso_lam) ):
+                    coeff_new[biginds]   = temper.copy()
+                    coeff_new[smallinds] = 0
                     frac_iter = frac_iter + 1
                     break
 
-                temp[0, biginds]   = temper[0,:].copy() # copy to old
+                temp[biginds]   = temper.copy() # copy to old
 
-            coeff_new[0,biginds]   = temper[0,:].copy() # new coeffs value
-            coeff_new[0,smallinds] = 0                  # new coeffs value
+            coeff_new[biginds]   = temper.copy() # new coeffs value
+            coeff_new[smallinds] = 0                  # new coeffs value
 
-        biginds     = np.array(biginds)
         # check for convergence
-        if( (np.linalg.norm(coeff_new[0,:] - coeff_old[0,:], np.inf) < tol) and np.all(support == biginds) ):
+        if( (np.linalg.norm(coeff_new - coeff_old, np.inf) < tol) and np.all(support == biginds) ):
             if(print_flag):
                 print(" converged at iteration ", iteration,  " frac_iter ", frac_iter/(iteration+1), " flag ", htp_flag)
-            return coeff_new.T
+            return coeff_new
 
         # copy the values between old and new
-        support           = biginds.copy()
-        coeff_old[0,:]    = coeff_new[0,:].copy()
+        support = biginds.copy()
+        coeff_old = coeff_new.copy()
 
-    return coeff_new.T #return a column vector
+    return coeff_new #return a column vector
